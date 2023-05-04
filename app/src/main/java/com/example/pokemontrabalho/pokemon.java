5d368ffcb3c1fdc534fd1a70f80b6a043d82e66c
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2023-05-04 20:32:37 -0300

    Adcionado conteudo pokemon.xml

--- app/src/main/java/com/example/pokemontrabalho/pokemon.java
+++ app/src/main/java/com/example/pokemontrabalho/pokemon.java
@@ -2,7 +2,10 @@ package com.example.pokemontrabalho;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class pokemon extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class pokemon extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemon);
+
     }
+
+
 }
